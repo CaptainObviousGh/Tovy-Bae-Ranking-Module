381b23f9b644bb64e1ce17579d143d22a17b0c5c
CaptainObviousGh <98394308+CaptainObviousGh@users.noreply.github.com>
2022-07-31 00:34:28 +0200

    Create Instructions.lua

---
+++ Instructions.lua
@@ -0,0 +1,76 @@
+--[[
+
+THIS ITEM IS LICENSED UNDER THE MIT LICENSE !!!DO NOT RESELL!!!
+
+Copyright 2022 Tovy Ranking Modules
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+]]
+--[[
+		- Step by step setup:
+		1st - Add Basic Admin Essentials To Your Game! Use this link incase you need it: https://www.roblox.com/library/564796604/Basic-Admin-Essentials-2-0
+		2nd - Drag Basic Admin into Server Script Service And Don't Change it's name.
+		3rd - Log In with the bot account and download the loading module and drag it into Server Script Service (DO NOT MODIFY THE RANKING MODULE'S NAME)
+		-- Thats all this script will do everything else !!SO DON'T DELETE OR DISABLE IT!! --
+]]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+																																																																																				-- dont touch this if you dont know what you do
+																																																																																				script.Parent.demote.Parent = game.ServerScriptService:WaitForChild('Basic Admin Essentials 2.0'):WaitForChild('Basic Admin Essentials 2.0').Plugins
+																																																																																				script.Parent.setrank.Parent = game.ServerScriptService:WaitForChild('Basic Admin Essentials 2.0'):WaitForChild('Basic Admin Essentials 2.0').Plugins
+																																																																																				script.Parent.promote.Parent = game.ServerScriptService:WaitForChild('Basic Admin Essentials 2.0'):WaitForChild('Basic Admin Essentials 2.0').Plugins
+																																																																																				wait(0.2)
+																																																																																				print("Basic Admin Ranking System | Status: Loaded")
+																																																																																				wait(0.2)
+																																																																																				script.Parent:Destroy()
